0663a18fec2c6baa0f4cd8bc6e5686afb4fbf50f
Andrew Tsoi <thtsoi8@gmail.com>
2021-12-11 22:03:01 +0800

    Day 2 code

---
+++ src/kotlin/Day02.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var depth = 0
+        var horizontalPosition = 0
+        val operations = input.map{ it.split(' ') }
+        for ((direction, amountString) in operations) {
+            val amount = amountString.toInt()
+            when (direction) {
+                "up" -> depth-=amount
+                "down" -> depth+=amount
+                "forward" -> horizontalPosition+=amount
+            }
+        }
+        return depth * horizontalPosition
+    }
+
+    fun part2(input: List<String>): Int {
+        var depth = 0
+        var horizontalPosition = 0
+        var aim = 0
+        val operations = input.map{ it.split(' ') }
+        for ((direction, amountString) in operations) {
+            val amount = amountString.toInt()
+            when (direction) {
+                "up" -> aim-=amount
+                "down" -> aim+=amount
+                "forward" -> {
+                    horizontalPosition+=amount
+                    depth+=amount*aim
+                }
+            }
+        }
+        return depth * horizontalPosition
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+
+    val input = readInput("Day02")
+    println(part1(input))
+
+    check(part2(testInput) == 900)
+    println(part2(input))
+}
