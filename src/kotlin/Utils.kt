086e9846f87156729bf4d63f58fa69b9f9f87f98
Andrew Tsoi <thtsoi8@gmail.com>
2021-12-12 10:21:11 +0800

    fix Day 1 code

--- src/kotlin/Utils.kt
+++ src/kotlin/Utils.kt
@@ -7,6 +7,9 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src/resources", "$name.txt").readLines()
 
+fun readInputAsInt(name: String) = File("src/resources", "$name.txt").readLines().map{ it.toInt() }
+
+
 /**
  * Converts string to md5 hash.
  */
